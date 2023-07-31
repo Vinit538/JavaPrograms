b1113c93342ea821e7df944fd9f2aefe787090a2
Vinit538 <vinitkaratagi538@gmail.com>
2023-07-31 11:10:46 +0530

    updated

--- JavaPrograms/src/Collection_Programs/ComparatorClass.java
+++ JavaPrograms/src/Collection_Programs/ComparatorClass.java
@@ -1,28 +1,32 @@
 package Collection_Programs;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class ComparatorClass{
 	public static void main(String[] args) {
+		// array List with data 
 		ArrayList<Object> al=new ArrayList<>();
 		al.add(new Employee(100, "Ramesh", 100000));
 		al.add(new Employee(50, "Suresh", 500000));
 		al.add(new Employee(150, "Mahesh", 150000));
 		System.out.println(al);
 		System.out.println();
+		//Creating object of the alpha class
 		Alpha alpha=new Alpha();
+		//Calling sort method which is present in Collections and passing to parameter arrayList, alpha class the method to sort 
 		Collections.sort(al,alpha);
 		System.out.println(al);
 		
 	}
 	
 }
+//Class alpha implements comparator Generic type of object 
 class Alpha implements Comparator<Object>
 {
 
+	//method the sort by id of the employee
 	@Override
 	public int compare(Object x, Object y) {
 		if(((Employee)(x)).id > ((Employee)(y)).id)
